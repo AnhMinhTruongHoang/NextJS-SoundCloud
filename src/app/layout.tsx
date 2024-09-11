f3f56a08b7ec62e146df18071ce9d708f65c356b
AnhMinhTruongHoang <truonghoanganhminh2000@gmail.com>
2024-09-12 01:32:06 +0700

    ok

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import AppFooter from "@/components/footer/app.footer";
 import AppHeader from "@/components/header/app.header";
 import ThemeRegistry from "@/components/theme-registry/theme.registry";
 
@@ -12,6 +13,7 @@ export default function RootLayout({
         <ThemeRegistry>
           <AppHeader />
           {children}
+          <AppFooter/>
         </ThemeRegistry>
       </body>
     </html>
---
+++ src/components/footer/app.footer.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { useHasMounted } from "@/unity/customHooks";
+import { AppBar, Container } from "@mui/material";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+
+const AppFooter = () => {
+  const hasMounted = useHasMounted();
+
+  if (!hasMounted) {
+    return <> </>;
+  }
+  return (
+    <div>
+      <AppBar
+        position="fixed"
+        sx={{ top: "auto", bottom: 0, background: "#f2f2f2" }}
+      >
+        <Container sx={{ display: "flex", gap: 10 }}>
+          <AudioPlayer
+            src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3"
+            volume={0.5}
+            style={{ boxShadow: "unset", background: "#f2f2f2" }}
+          />
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "start",
+              justifyContent: "center",
+              minWidth: 100,
+            }}
+          >
+            <div style={{ color: "#ccc" }}>Eric</div>
+            <div style={{ color: "black" }}>Who am I ?</div>
+          </div>
+        </Container>
+      </AppBar>
+    </div>
+  );
+};
+
+export default AppFooter;
